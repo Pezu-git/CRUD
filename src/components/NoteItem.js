c4b16532c77c5677ba8549bf9f0526a6921128cf
Pezu-git <artem_nikolaev69@mail.ru>
2021-10-22 11:22:39 +0300

    Ref

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,15 +1,13 @@
-import React, {useRef} from 'react';
+import React from 'react';
 
 const NoteItem = ({id, text, deleteNote}) => {
-  const ref = useRef();
   const onClickHandler = () => {
-    const {id} = ref.current.dataset;
     deleteNote(id);
   }
 
   return (
     <div className="col-4">
-      <div ref={ref} className="card mb-2"  data-id={id}>
+      <div className="card mb-2"  data-id={id}>
         <div className="card-body">
           <p className="card-text">{text}</p>
           <button className="btn btn-danger" onClick={onClickHandler}>Удалить</button>
